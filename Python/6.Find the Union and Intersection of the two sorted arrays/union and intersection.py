6a67d96334cc4b2615f969e1584af0e562ccb909
Keerthisriii <amudalakeerthisri6@gmail.com>
2024-07-03 19:37:48 +0530

    03-07-2024

---
+++ Python/6.Find the Union and Intersection of the two sorted arrays/union and intersection.py
@@ -0,0 +1,24 @@
+set1 = {1,2,3,4}
+set2 = {3,4,5,6}
+
+#union 
+
+print(set1.union(set2))
+
+
+
+#Intersection
+
+print(set1.intersection(set2))
+
+#difference
+
+print(set1.difference(set2))
+
+
+
+
+
+# summary : use type casting to convert list into set
+# arr1[]=1,2,3,4,5
+# var_name=set(arr1)   #typecasted
