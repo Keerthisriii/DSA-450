74890b49cb52a4c72fcf27e215478b701fe29631
Keerthisriii <amudalakeerthisri6@gmail.com>
2024-07-03 19:38:56 +0530

    Update moveNEG.py

--- Python/5.Move all the negative elements to end  of array/moveNEG.py
+++ Python/5.Move all the negative elements to end  of array/moveNEG.py
@@ -2,12 +2,12 @@ arr=[10,20,-1,-3,22,40]
 arr1 = []
 n = len(arr)
 for i in range(n):
-    if arr[i]>0:
+    if arr[i]>=0:
         arr1.append(arr[i])
         
         
 for i in range(n):
-    if arr[i]<=0:
+    if arr[i]<0:
         arr1.append(arr[i])
             
 for i in range(n):
